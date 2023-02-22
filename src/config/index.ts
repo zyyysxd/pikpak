0735c528513109986549170ee0610ac542a96a13
去年夏天 <tjsky@foxmail.com>
2023-02-22 20:17:28 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ export const proxy = [
   'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://pan.yooyi.tk',
   'https://pk.pikpakz.top',
   'https://cc123.cf',
   'https://cf-api.xswd.cf',
