5df46517fd44261f0a93831c88e968056608a43f
去年夏天 <tjsky@foxmail.com>
2023-07-03 10:04:06 +0800

    移除失效反代
    移除失效反代

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,13 +2,11 @@ export const proxy = [
   'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://pk.pikpakz.top',
   'https://cc123.cf',
   'https://cf-api.xswd.cf',
   'https://wkr.btstream.net',
   'https://dns.firmant.me',
   'https://pikpak.yang-s.online',
-  'https://pikpak.hongtonlow.ga',
   'https://pikpak.520936.xyz',
 ]
 
