8cc0079b7f4f76a5b7c4d33f9f41e6f59025deeb
去年夏天 <tjsky@foxmail.com>
2023-09-02 17:46:50 +0800

    去除失效反代并更新新的反代

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,12 @@ export const proxy = [
   'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://cc123.cf',
   'https://cf-api.xswd.cf',
   'https://wkr.btstream.net',
   'https://pikpak.520936.xyz',
+  'https://wkr.btstream.net',
+  'https://darazemon.xyz',
+  'https://pikpak.995579.xyz',
 ]
 
-export const version = '1.25.5'
+export const version = '1.26.2'
