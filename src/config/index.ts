bd83b9bf5da43c365304026b95daa4ee51c46e16
去年夏天 <admin@tjsky.net>
2024-03-18 18:02:52 +0800

    Update index.ts
    去除失效反代

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,5 @@
 export const proxy = [
-  'https://diii.tk',
-  'https://api-pikpak.tjsky.cf',
-  'https://api-pikpak.tjsky-1.cf',
-  'https://cf-api.xswd.cf',
-  'https://wkr.btstream.net',
-  'https://pikpak.520936.xyz',
-  'https://wkr.btstream.net',
-  'https://darazemon.xyz',
-  'https://pikpak.995579.xyz',
+  'https://pikpak.995579.xyzk'
 ]
 
 export const version = '1.26.2'
